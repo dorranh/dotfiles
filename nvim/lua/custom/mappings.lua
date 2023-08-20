be51693b43758f66556bb70ca0c2efa7e2a7a7e7
Dorran Howell <dorranh@appalachian.dev>
2023-08-20 10:50:38 +0200

    Add mapping for toggling window transparency

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -4,6 +4,7 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>ts"] = {function() require('base46').toggle_transparency() end, "toggle transparency"}
   },
 }
 
