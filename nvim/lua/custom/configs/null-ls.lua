a4802739a33f15ca5a92f6686160c66fa9767961
Dorran Howell <dorranh@appalachian.dev>
2023-09-18 11:17:19 +0200

    Enable format on save

--- nvim/lua/custom/configs/null-ls.lua
+++ nvim/lua/custom/configs/null-ls.lua
@@ -15,7 +15,25 @@ local sources = {
   b.formatting.clang_format,
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup {
   debug = true,
   sources = sources,
+  -- Enable formatting on save
+  -- See: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+          vim.lsp.buf.format { async = false }
+        end,
+      })
+    end
+  end,
 }
