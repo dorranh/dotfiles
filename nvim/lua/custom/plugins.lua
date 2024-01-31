fa96a929b5975ea8c453875b3fefbfb7112418a4
Dorran Howell <dorran.howell@tweag.io>
2024-01-31 12:22:30 +0100

    Add smartcolumn to nvim config

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -1,4 +1,4 @@
-local overrides = require("custom.configs.overrides")
+local overrides = require "custom.configs.overrides"
 
 ---@type NvPluginSpec[]
 local plugins = {
@@ -25,7 +25,7 @@ local plugins = {
   -- override plugin configs
   {
     "williamboman/mason.nvim",
-    opts = overrides.mason
+    opts = overrides.mason,
   },
 
   {
@@ -47,132 +47,144 @@ local plugins = {
     end,
   },
 
--- begin metals
   {
-     "scalameta/nvim-metals",
-      dependencies = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
-      ft = { "scala", "sbt" },
-      event = "BufEnter *.worksheet.sc",
-      config = function()
-        local api = vim.api
-        ----------------------------------
-        -- OPTIONS -----------------------
-        ----------------------------------
-        -- global
-        vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
-        vim.opt_global.shortmess:remove("F")
-        vim.opt_global.shortmess:append("c")
-        -- LSP Setup ---------------------
-        ----------------------------------
-        local metals_config = require("metals").bare_config()
+    "m4xshen/smartcolumn.nvim",
+    event = { "InsertEnter" },
+    opts = {
+      colorcolumn = "100",
+      disabled_filetypes = {
+        "nvdash",
+        "help",
+      },
+    },
+  },
 
-        -- Example of settings
-        metals_config.settings = {
-          showImplicitArguments = true,
-          showInferredType = true,
-          superMethodLensesEnabled = true,
-          showImplicitConversionsAndClasses = true,
-        }
+  -- begin metals
+  {
+    "scalameta/nvim-metals",
+    dependencies = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
+    ft = { "scala", "sbt" },
+    event = "BufEnter *.worksheet.sc",
+    config = function()
+      local api = vim.api
+      ----------------------------------
+      -- OPTIONS -----------------------
+      ----------------------------------
+      -- global
+      vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
+      vim.opt_global.shortmess:remove "F"
+      vim.opt_global.shortmess:append "c"
+      -- LSP Setup ---------------------
+      ----------------------------------
+      local metals_config = require("metals").bare_config()
 
-        -- *READ THIS*
-        -- I *highly* recommend setting statusBarProvider to true, however if you do,
-        -- you *have* to have a setting to display this in your statusline or else
-        -- you'll not see any messages from metals. There is more info in the help
-        -- docs about this
-        metals_config.init_options.statusBarProvider = "on"
+      -- Example of settings
+      metals_config.settings = {
+        showImplicitArguments = true,
+        showInferredType = true,
+        superMethodLensesEnabled = true,
+        showImplicitConversionsAndClasses = true,
+      }
 
-        -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
-        metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- *READ THIS*
+      -- I *highly* recommend setting statusBarProvider to true, however if you do,
+      -- you *have* to have a setting to display this in your statusline or else
+      -- you'll not see any messages from metals. There is more info in the help
+      -- docs about this
+      metals_config.init_options.statusBarProvider = "on"
 
-        -- Debug settings if you're using nvim-dap
-        local dap = require("dap")
+      -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        dap.configurations.scala = {
-          {
-            type = "scala",
-            request = "launch",
-            name = "RunOrTest",
-            metals = {
-              runType = "runOrTestFile",
-              --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
-            },
+      -- Debug settings if you're using nvim-dap
+      local dap = require "dap"
+
+      dap.configurations.scala = {
+        {
+          type = "scala",
+          request = "launch",
+          name = "RunOrTest",
+          metals = {
+            runType = "runOrTestFile",
+            --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
           },
-          {
-            type = "scala",
-            request = "launch",
-            name = "Test Target",
-            metals = {
-              runType = "testTarget",
-            },
+        },
+        {
+          type = "scala",
+          request = "launch",
+          name = "Test Target",
+          metals = {
+            runType = "testTarget",
           },
-        }
+        },
+      }
 
-        metals_config.on_attach = function(client, bufnr)
-          local metals = require("metals")
-          local utils = require("core.utils")
-          utils.load_mappings("lspconfig", {buffer = bufnr})
-          metals.setup_dap()
+      metals_config.on_attach = function(client, bufnr)
+        local metals = require "metals"
+        local utils = require "core.utils"
+        utils.load_mappings("lspconfig", { buffer = bufnr })
+        metals.setup_dap()
 
-          local wk = require("which-key")
-          wk.register({
-            ["<localleader>"] = {
-              h = {
-                name = "hover",
-                c = {
-                  function()
-                    metals.toggle_setting("showImplicitConversionsAndClasses")
-                  end,
-                  "Toggle show implicit conversions and classes",
-                },
-                i = {
-                  function()
-                    metals.toggle_setting("showImplicitArguments")
-                  end,
-                  "Toggle show implicit arguments",
-                },
-                t = {
-                  function()
-                    metals.toggle_setting("showInferredType")
-                  end,
-                  "Toggle show inferred type",
-                },
+        local wk = require "which-key"
+        wk.register({
+          ["<localleader>"] = {
+            h = {
+              name = "hover",
+              c = {
+                function()
+                  metals.toggle_setting "showImplicitConversionsAndClasses"
+                end,
+                "Toggle show implicit conversions and classes",
+              },
+              i = {
+                function()
+                  metals.toggle_setting "showImplicitArguments"
+                end,
+                "Toggle show implicit arguments",
               },
               t = {
-                name = "Tree view",
-                t = {
-                  function()
-                    require("metals.tvp").toggle_tree_view()
-                  end,
-                  "Toggle tree view",
-                },
-                r = {
-                  function()
-                    require("metals.tvp").reveal_in_tree()
-                  end,
-                  "Review in tree view",
-                },
-              },
-              w = {
                 function()
-                  metals.hover_worksheet({ border = "single" })
+                  metals.toggle_setting "showInferredType"
                 end,
-                "Hover worksheet",
+                "Toggle show inferred type",
               },
             },
-          }, {
-            buffer = bufnr,
-          })
-          wk.register({
-            ["<localleader>t"] = {
-              function()
-                metals.type_of_range()
-              end,
-              "Type of range",
+            t = {
+              name = "Tree view",
+              t = {
+                function()
+                  require("metals.tvp").toggle_tree_view()
+                end,
+                "Toggle tree view",
+              },
+              r = {
+                function()
+                  require("metals.tvp").reveal_in_tree()
+                end,
+                "Review in tree view",
+              },
             },
-          }, {
-            mode = "v",
-            buffer = bufnr,
-          })
+            w = {
+              function()
+                metals.hover_worksheet { border = "single" }
+              end,
+              "Hover worksheet",
+            },
+          },
+        }, {
+          buffer = bufnr,
+        })
+        wk.register({
+          ["<localleader>t"] = {
+            function()
+              metals.type_of_range()
+            end,
+            "Type of range",
+          },
+        }, {
+          mode = "v",
+          buffer = bufnr,
+        })
       end
 
       local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
@@ -187,24 +199,23 @@ local plugins = {
         group = nvim_metals_group,
       })
     end,
-}
- 
+  },
+
   -- end metals
-
 }
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+-- To make a plugin not be loaded
+-- {
+--   "NvChad/nvim-colorizer.lua",
+--   enabled = false
+-- },
 
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
-  --
+-- All NvChad plugins are lazy-loaded by default
+-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+-- {
+--   "mg979/vim-visual-multi",
+--   lazy = false,
+-- }
+--
 
 return plugins
