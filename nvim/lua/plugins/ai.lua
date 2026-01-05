323a56ae3fdff7b5f5dbe937b90acd71409d854a
Dorran Howell <dorranh@appalachian.dev>
2026-01-05 09:06:48 +0100

    Use Ollama as default minuet completion provider
    This was previously using Gemini which now has more restricted API
    access for the free tier.

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -4,7 +4,22 @@ return {
 		"milanglacier/minuet-ai.nvim",
 		config = function()
 			require("minuet").setup({
-				provider = "gemini",
+				provider = "openai_compatible",
+				provider_options = {
+					openai_compatible = {
+						-- Ollama local API
+						end_point = "http://localhost:11434/v1/chat/completions",
+						-- A dummy or placeholder API key required by the plugin (Ollama doesn’t enforce it)
+						api_key = "TERM",
+						-- Model name defined in Ollama
+						model = "gemma3:12b",
+						-- Optional parameters passed to the model
+						optional = {
+							max_tokens = 1024,
+							temperature = 0.2,
+						},
+					},
+				},
 			})
 		end,
 	},
